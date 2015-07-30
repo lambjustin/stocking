1fd1917b9708ea77fd372dd5c799186599a57221
Justin Lamb <justindlamb@gmail.com>
2015-07-30 17:09:29 -0400

    added who is signed in functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for Devise
+  #REMEMBER to set this to actual host!
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
